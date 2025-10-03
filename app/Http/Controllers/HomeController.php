5d4dcd978901b5bcc57a4e9e2de591bdd457414b
ramadhani24si-framework <ramadhani24si@mahasiswa.pcr.ac.id>
2025-10-03 08:14:18 +0700

    ubah remote home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,7 +12,7 @@ class HomeController extends Controller
     public function index()
     {
         $data ['username']        = 'adricy';
-    $data ['last_login']      = date('Y-m-d H:i:s');
+    $data ['remote']      = date('github');
     $data ['list_pendidikan'] = ['SD','SMP','SMA','S1','S2','S3'];
     return view('home', $data );
     }
