b5f2119d5dfed6fd0e528ba5874dce15f5474ed8
faqih2sib <faqih24si@mahasiswa.pcr.ac.id>
2025-10-24 10:53:36 +0700

    perubahan

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\PegawaiController;
 use App\Http\Controllers\QuestionController;
-use App\Http\Controllers\DashboardControoller;
+use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\PelangganController;
+use App\Http\Controllers\DashboardControoller;
 Route::get('/', function () {
     return view('welcome');
 });
@@ -35,3 +36,6 @@ Route::post('question/store', [QuestionController::class, 'store'])
 Route::get('dashboard', [DashboardControoller::class, 'index'])
     ->name('dashboard');
 Route::resource('pelanggan', PelangganController::class);
+
+Route::resource('user', UserController::class);
+
